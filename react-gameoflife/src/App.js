ebc637f6ccdea35594e0fe66057e7e1a9ee827bf
Tan Yan-Hao Joshua <joshuatanyanhao@gmail.com>
2023-04-14 03:13:52 +0800

    Create the board
    Added the Game component and created the grlines by implementing a
    linear-gradient in the Game.css file.
    
    Updated App.js to display the game board.

--- react-gameoflife/src/App.js
+++ react-gameoflife/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Game from './Game'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Conway's Game of Life.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Game/>
       </header>
     </div>
   );
